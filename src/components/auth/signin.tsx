d8d9e4336e0a6b891bda5fb97706ea2e8aed9d94
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-05-26 18:51:15 -0300

    Criando o componente de Input

--- src/components/auth/signin.tsx
+++ src/components/auth/signin.tsx
@@ -4,6 +4,7 @@ import { faHeart } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { Input } from "../ui/input";
 
 export const SigninForm = () => {
     const router = useRouter();
@@ -16,17 +17,16 @@ export const SigninForm = () => {
 
     return (
         <>
-            <input
-                type="email"
+            <Input
                 placeholder="Digite seu e-mail"
                 value={emailField}
-                onChange={e => setEmailField(e.target.value)}
+                onChange={t => setEmailField(t)}
             />
-            <input
-                type="password"
+            <Input
                 placeholder="Digite sua senha"
                 value={passwordField}
-                onChange={e => setPasswordField(e.target.value)}
+                onChange={t => setPasswordField(t)}
+                password
             />
 
             <button onClick={handleEnterButton}>Entrar</button>
