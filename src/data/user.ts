5739ab522c482c8861b7ae927db5542c452f7d6b
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-05-27 08:33:46 -0300

    criando usuário fake

---
+++ src/data/user.ts
@@ -0,0 +1,11 @@
+import { User } from "@/types/user";
+
+export const user: User = {
+    slug: 'jooluisrm',
+    name: 'João Luís',
+    avatar: 'https://ui-avatars.com/api?background=random&name=Bonieky&size=100',
+    cover: 'https://img.freepik.com/fotos-gratis/linda-garota-de-pe-no-barco-e-olhando-para-as-montanhas-na-barragem-de-ratchaprapha-no-parque-nacional-khao-sok-provincia-de-surat-thani-tailandia_335224-849.jpg?ga=GA1.1.325488144.1748311239&semt=ais_hybrid&w=740',
+    bio: 'Desenvolvedor Front-end Júnior',
+    link: 'https://b7web.com.br',
+    postCount: 127
+}
