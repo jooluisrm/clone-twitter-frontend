000d23db358c47442278806dcf0d467a8a60f1e0
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-05-28 10:17:57 -0300

    Criando página de um tweet individual

--- src/components/tweet/tweet-item.tsx
+++ src/components/tweet/tweet-item.tsx
@@ -10,9 +10,10 @@ import { useState } from "react";
 
 type Props = {
     tweet: Tweet;
+    hideComments?: boolean;
 }
 
-export const TweetItem = ({ tweet }: Props) => {
+export const TweetItem = ({ tweet, hideComments }: Props) => {
 
     const [liked, setLiked] = useState(tweet.liked);
 
@@ -30,7 +31,7 @@ export const TweetItem = ({ tweet }: Props) => {
             <div className="flex-1">
                 <div className="flex flex-wrap items-center gap-x-3">
                     <div className="font-bold text-lg">
-                        <Link href={`/${tweet.user.slug}`}> 
+                        <Link href={`/${tweet.user.slug}`}>
                             {tweet.user.name}
                         </Link>
                     </div>
@@ -47,16 +48,19 @@ export const TweetItem = ({ tweet }: Props) => {
                     </div>
                 }
                 <div className="flex mt-6 text-gray-500">
-                    <div className="flex-1">
-                        <Link href={`/tweet/${tweet.id}`}>
-                            <div className="inline-flex items-center gap-2 cursor-pointer">
-                                <FontAwesomeIcon icon={faComment} className="size-6" />
-                                <div className="text-lg">
-                                    {tweet.commentCount}
+                    {!hideComments &&
+                        <div className="flex-1">
+                            <Link href={`/tweet/${tweet.id}`}>
+                                <div className="inline-flex items-center gap-2 cursor-pointer">
+                                    <FontAwesomeIcon icon={faComment} className="size-6" />
+                                    <div className="text-lg">
+                                        {tweet.commentCount}
+                                    </div>
                                 </div>
-                            </div>
-                        </Link>
-                    </div>
+                            </Link>
+                        </div>
+                    }
+
                     <div className="flex-1">
                         <div className="inline-flex items-center gap-2 cursor-pointer">
                             <FontAwesomeIcon icon={faRetweet} className="size-6" />
